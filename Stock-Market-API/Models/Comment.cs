da4ec51fba8d4274675ed9dd2a0f6429684ebc44
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-04-02 16:39:14 +0300

    Create Comment Class

--- Stock-Market-API/Models/Comment.cs
+++ Stock-Market-API/Models/Comment.cs
@@ -2,5 +2,13 @@
 {
     public class Comment
     {
+        public int Id { get; set; }
+        public int StockId { get; set; }
+
+        // Navigation Property
+        public Stock Stock { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
